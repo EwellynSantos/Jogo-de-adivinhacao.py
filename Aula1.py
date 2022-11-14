9f1d831a9eb0f1dc154dd96f73f6f5dd5a060aa6
Millena <ewellynsnt@icloud.com>
2022-11-13 22:01:45 -0300

    novo commit

--- Aula1.py
+++ Aula1.py
@@ -5,27 +5,43 @@ import random
 print("Bem vindos ao jogo de adivinhação!")
  
 nSecreto = random.randrange(1,51)
-totalTentativas = 1
+totalTentativas = 0
+pontos = 100
 
-print(nSecreto)
+print("Escolha o nível de dificuldade:")
+print("(1) Fácil (2) Médio (3) Difícil")
 
-for rodada in range(totalTentativas, 4, 1):
-        print(f"Tentativa: {totalTentativas} de 3")
+nivel = int(input("Defina o nível:  "))
+print (nSecreto)
+if (nivel == 1):
+        totalTentativas = 10
+elif (nivel == 2):
+        totalTentativas = 5
+elif (nivel == 3):
+        totalTentativas = 3
+
+
+for rodada in range(1,totalTentativas + 1):
+        print("Tentativa {} de {}".format(rodada, totalTentativas))
 
         nUsuario = int(input("Digite um número entre 1 e 50: "))
         print("Você digitou ", nUsuario)
 
-        if(nUsuario < 1 and  nUsuario > 50):
+        if(nUsuario < 1 or  nUsuario > 50):
                 print("Você precisa digitar um número entre 1 e 50!")
-
+                continue
 
         if (nSecreto == nUsuario):
-                print("Você acertou!")
+                print("Você acertou e fez {} pontos!" .format(pontos))
                 break
-        elif(nUsuario > nSecreto):
-                print("Você errou! Voce chutou um número maior que o secreto.")
         else:
-                print("Você errou! Voce chutou um número menor que o secreto.")
+                if(nUsuario > nSecreto):
+                        print("Você errou! Voce chutou um número maior que o secreto.")
+                elif(nUsuario < nSecreto):
+                        print("Você errou! Voce chutou um número menor que o secreto.")
+                pontosPerdidos = abs(nSecreto - nUsuario)
+                pontos = pontos - pontosPerdidos
+        
 
 
         totalTentativas = totalTentativas + 1
